b5b24a27d315a1d8b0704f820701998f5d17bacd
Fileurix3 <k96260831@gmail.com>
2025-02-06 10:59:06 +0200

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
-import * as cookieParser from "cookie-parser";
 import { S3Service } from "./s3/s3.service";
+import { SwaggerModule } from "@nestjs/swagger";
+import * as cookieParser from "cookie-parser";
+import * as YAML from "yamljs";
+import * as path from "path";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const document = YAML.load(path.join(__dirname, "../swagger.yaml"));
 
   app.useGlobalPipes(new ValidationPipe({ stopAtFirstError: true }));
   app.use(cookieParser());
+
+  SwaggerModule.setup("api/docs", app, document);
 
   const s3Service = app.get(S3Service);
   await s3Service.checkAndCreateBucket();
@@ -16,6 +22,7 @@ async function bootstrap() {
   const PORT = 3000;
   await app.listen(PORT, async () => {
     console.log(`Serves has been started on PORT ${PORT}`);
+    console.log(`Documentation in swagger: /api/docs`);
   });
 }
 bootstrap();
